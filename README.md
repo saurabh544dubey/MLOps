cd52bb4d57e0e8ae5fffaac53a24332677328b3f
Saurabh Dubey <Saurabhbcp2122@gmail.com>
2024-01-14 13:49:56 +0530

    Setup and Requirements

--- README.md
+++ README.md
@@ -2,5 +2,9 @@
 1. New Environment : Create and activate an environment first
 2. Initialize and setup the repository
 3. Create .gitignore in repository
-4. Create setup.py -- It'll be responsible for creating my ML Application as a package
-3. Create requirements.txt -- store packages to be used
+4. Create setup.py -- It'll be responsible for creating my ML Application as a package.
+5. Create folder `src` and under it file `__init__.py`: This can be imported like libraries. All the project development inside this folder.
+6. Create requirements.txt -- store packages to be used. At last, we'll mention `-e .` which will automatically trigger setup.py file.
+
+#### To install Libraries
+- `pip install -r requiremnts.txt`
--- setup.py
+++ setup.py
@@ -1,11 +1,37 @@
 from setuptools import find_packages,setup
+from typing import List
 
+HYPHER_E_DOT="-e ."
+### It'll fetch the libraries from requirements.txt file
+def get_requirements(file_path:str)->list[str]:
+    """This function will return the list of requirements
+
+    Args:
+        file_path (str): This is the path of the requirements file
+
+    Returns:
+        list[str]: It is returning a list of all packages to be installed  
+    """
+
+    requirements=[]
+    with open(file_path) as f:
+        requirements=f.readlines()
+        ### When we read lines this way, it'll also read \n, we have to replace it with  ""
+        requirements=[req.replace("\n","") for req in requirements]
+        
+        ### Because -e . is also read which triggers setup.py, we have to remove it from the installing list
+        if HYPHER_E_DOT in requirements:
+            requirements.remove(HYPHER_E_DOT)
+            
+    return requirements
+
+### This is basically information about Project
 setup(
     name="mlproject",
     version="0.0.1",
     author="Saurabh",
     author_email="Saurabhbcp2122@gmail.com",
-    packages=find_packages(),
-    install_requires=["pandas","numpy","seaborn"]
+    packages=find_packages(),   ## It'll consider src as a package itself
+    install_requires=get_requirements("requirements.txt")
 )
-# 21:00
+
